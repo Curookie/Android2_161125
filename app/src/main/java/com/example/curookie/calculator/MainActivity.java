1ee501029e123ea6e20c478779d1b140d67142dc
Curookie <curookie@naver.com>
2016-11-17 11:18:22 +0900

    +(더하기) 버튼 구현

--- app/src/main/java/com/example/curookie/calculator/MainActivity.java
+++ app/src/main/java/com/example/curookie/calculator/MainActivity.java
@@ -2,24 +2,36 @@ package com.example.curookie.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button sum;
+    EditText num1,num2;
+    Button sum,sub,mul,div;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        num1 = (EditText)findViewById(R.id.num1);
+        num2 = (EditText)findViewById(R.id.num2);
+
         sum();
-        
+
     }
 
     private void sum() {
         sum = (Button)findViewById(R.id.sum);
-
+        sum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(),Integer.parseInt(num1.getText().toString())+Integer.parseInt(num2.getText().toString()),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
