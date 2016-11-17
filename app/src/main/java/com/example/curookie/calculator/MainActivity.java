b9c2b48b076f16664aaf1ba312cde6c116a30519
Curookie <curookie@naver.com>
2016-11-17 11:07:06 +0900

    UI 구현

--- app/src/main/java/com/example/curookie/calculator/MainActivity.java
+++ app/src/main/java/com/example/curookie/calculator/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.curookie.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button sum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sum();
+        
     }
+
+    private void sum() {
+        sum = (Button)findViewById(R.id.sum);
+
+    }
+
 }
