87f6f922c6cfa94c1eb404906633a4eb6c5fc8f5
Curookie <curookie@naver.com>
2016-11-18 16:20:57 +0900

    수업내용 완료

--- app/src/main/java/com/example/curookie/calculator/Main2Activity.java
+++ app/src/main/java/com/example/curookie/calculator/Main2Activity.java
@@ -9,12 +9,13 @@ import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
     CheckBox cb;
-    RadioButton rb1, rb2, rb3;
+    RadioGroup rg;
     Button btnSelect;
     ImageView iv;
     LinearLayout ll;
@@ -26,6 +27,15 @@ public class Main2Activity extends AppCompatActivity {
         setTitle("애완동물 사진 보기");
 
         initId();
+
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(rg.getCheckedRadioButtonId()==R.id.radioButton) iv.setImageResource(R.drawable.dog);
+                else if(rg.getCheckedRadioButtonId()==R.id.radioButton2) iv.setImageResource(R.drawable.cat);
+                else if(rg.getCheckedRadioButtonId()==R.id.radioButton3) iv.setImageResource(R.drawable.rabbit);
+            }
+        });
 
         cb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -43,11 +53,9 @@ public class Main2Activity extends AppCompatActivity {
 
     private void initId() {
         cb = (CheckBox) findViewById(R.id.checkBox);
-        rb1 = (RadioButton) findViewById(R.id.radioButton);
-        rb2 = (RadioButton) findViewById(R.id.radioButton2);
-        rb3 = (RadioButton) findViewById(R.id.radioButton3);
         btnSelect = (Button) findViewById(R.id.btnSelect);
         iv = (ImageView) findViewById(R.id.imageView);
         ll = (LinearLayout)findViewById(R.id.linear);
+        rg = (RadioGroup) findViewById(R.id.rg);
     }
 }
