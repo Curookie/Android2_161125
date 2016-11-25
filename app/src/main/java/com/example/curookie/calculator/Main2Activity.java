5309ae46a34dde773dbf9a20c811d6ff845012ae
Curookie <curookie@naver.com>
2016-11-25 17:07:47 +0900

    오류 수정, 메뉴바 구현

--- app/src/main/java/com/example/curookie/calculator/Main2Activity.java
+++ app/src/main/java/com/example/curookie/calculator/Main2Activity.java
@@ -1,7 +1,10 @@
 package com.example.curookie.calculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -49,6 +52,38 @@ public class Main2Activity extends AppCompatActivity {
         });
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0,100,0, "계산기");
+        menu.add(0,101,0, "애완 동물 사진 보기");
+        menu.add(0,102,0, "안드로이드 사진 보기");
+        menu.add(0,103,0, "피자 주문");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 100 :
+                Intent intent = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case 101 :
+                Intent intent2 = new Intent(Main2Activity.this, Main2Activity.class);
+                startActivity(intent2);
+                break;
+            case 102 :
+                Intent intent3 = new Intent(Main2Activity.this, AndroidPhotoView.class);
+                startActivity(intent3);
+                break;
+            case 103 :
+                Intent intent4 = new Intent(Main2Activity.this, PizzaOrder.class);
+                startActivity(intent4);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void initId() {
--- app/src/main/java/com/example/curookie/calculator/PizzaOrder.java
+++ app/src/main/java/com/example/curookie/calculator/PizzaOrder.java
@@ -1,7 +1,10 @@
 package com.example.curookie.calculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -47,6 +50,38 @@ public class PizzaOrder extends AppCompatActivity {
                 tv3.setText(opt+"을 선택하셨습니다.");
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0,100,0, "계산기");
+        menu.add(0,101,0, "애완 동물 사진 보기");
+        menu.add(0,102,0, "안드로이드 사진 보기");
+        menu.add(0,103,0, "피자 주문");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 100 :
+                Intent intent = new Intent(PizzaOrder.this, MainActivity.class);
+                startActivity(intent);
+            break;
+            case 101 :
+                Intent intent2 = new Intent(PizzaOrder.this, Main2Activity.class);
+                startActivity(intent2);
+            break;
+            case 102 :
+                Intent intent3 = new Intent(PizzaOrder.this, AndroidPhotoView.class);
+                startActivity(intent3);
+                break;
+            case 103 :
+                Intent intent4 = new Intent(PizzaOrder.this, PizzaOrder.class);
+                startActivity(intent4);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void initId() {
