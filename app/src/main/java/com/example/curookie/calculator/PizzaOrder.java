169af50da35043115980e246419726f59e0fbe75
Curookie <curookie@naver.com>
2016-11-26 11:49:42 +0900

    직관적UI (Comma) 원 추가, 스크린샷 추가, 공백 처리, 앱 시작 액티비티 수정

--- app/src/main/java/com/example/curookie/calculator/PizzaOrder.java
+++ app/src/main/java/com/example/curookie/calculator/PizzaOrder.java
@@ -1,22 +1,28 @@
 package com.example.curookie.calculator;
 
 import android.content.Intent;
+import android.icu.text.DecimalFormat;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class PizzaOrder extends AppCompatActivity {
 
     Button btnSubmit;
     EditText or1,or2,or3;
     RadioGroup rg;
+    CheckBox cb1;
     ImageView iv;
     TextView tv1,tv2,tv3;
 
@@ -37,16 +43,25 @@ public class PizzaOrder extends AppCompatActivity {
         });
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onClick(View view) {
+                if(!or1.getText().toString().matches("^[0-9]+$")||!or2.getText().toString().matches("^[0-9]+$")||!or3.getText().toString().matches("^[0-9]+$")) {
+                    Toast.makeText(getApplicationContext(),"주문 항목을 입력해주세요~",Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 int o1 = Integer.parseInt(or1.getText().toString());
                 int o2 = Integer.parseInt(or2.getText().toString());
                 int o3 = Integer.parseInt(or3.getText().toString());
+                int sale = (int) Math.round((o1*16000+o2*11000+o3*4000)*0.93/10)*10;
                 String opt="피클";
                 if(rg.getCheckedRadioButtonId()==R.id.radioButton8) opt = "소스";
 
+                DecimalFormat df = new DecimalFormat("#,##0");
+
                 tv1.setText("주문 개수 : "+(o1+o2+o3));
-                tv2.setText("주문 금액 : "+(o1*16000+o2*11000+o3*4000));
+                if(cb1.isChecked()) tv2.setText("주문 금액 : "+df.format(sale)+"원");
+                else tv2.setText("주문 금액 : "+df.format((o1*16000+o2*11000+o3*4000))+"원");
                 tv3.setText(opt+"을 선택하셨습니다.");
             }
         });
@@ -90,6 +105,7 @@ public class PizzaOrder extends AppCompatActivity {
         or2 = (EditText) findViewById(R.id.order2);
         or3 = (EditText) findViewById(R.id.order3);
         rg = (RadioGroup) findViewById(R.id.option_rg);
+        cb1 = (CheckBox) findViewById(R.id.point);
         tv1 = (TextView) findViewById(R.id.ortxt1);
         tv2 = (TextView) findViewById(R.id.ortxt2);
         tv3 = (TextView) findViewById(R.id.ortxt3);
